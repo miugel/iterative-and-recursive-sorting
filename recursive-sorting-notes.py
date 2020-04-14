204f04c38ebe505ee9a8f2986f8a09f04dc7905b
Miguel <miguelqnicolas@gmail.com>
2020-04-14 16:34:26 -0400

    Clean up files and add recursive sorting notes

---
+++ recursive-sorting-notes.py
@@ -0,0 +1,12 @@
+'''
+recursion
+    fibonacci sequence
+    a base case (or cases) that specify when the  recursion terminates
+    a rule (or rules) that reduce all other cases towards a base case
+    calling a function inside of the function itself
+
+quick sort
+    pick a pivot
+
+merge sort
+'''
